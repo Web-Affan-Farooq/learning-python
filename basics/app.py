24edc35dd3890e1b720600c41fec7e6aa8adfb81
Web-Affan-Farooq <affanamir903@gmail.com>
2025-03-01 17:25:40 +0500

    completed string manipulation in python guide

---
+++ basics/app.py
@@ -0,0 +1,19 @@
+# String methods:
+
+# print(string.title()) # title cased string
+# print(string.upper())
+# print(string.lower())
+# print(string.capitalize()) # for first letter capitalize
+# print(string.split(" ")) 
+# print(string.count("o"))
+# print(string.rstrip())
+# print(string.isdigit());
+# print(string.isalnum()) # retuurns true if string contains characters A-Z or a-z or number 0-9
+# print(string.replace("hello","A"))  # for replacing a character or string
+# print(string.index("e"))  # for reaching index of a character in string
+# print(string.replace("e","A"))
+string = "            Hello world hello World           "
+print(string.rstrip())
+print(string.lstrip())
+print(string.strip())
+print(string.replace(" ", ""))
